126ced192b63360eca3d70e2b0ec60e5e684c62c
wang yuanhui <yuanhui.wang@thoughtworks.com>
2020-05-05 19:57:57 +0800

    add gradle dependency; add docker-compose DB; add yml file; add application class;

--- src/main/java/cn/xpbootcamp/gilded_rose/MainApplication.java
+++ src/main/java/cn/xpbootcamp/gilded_rose/MainApplication.java
@@ -1,4 +1,12 @@
 package cn.xpbootcamp.gilded_rose;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class MainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class, args);
+    }
 }
